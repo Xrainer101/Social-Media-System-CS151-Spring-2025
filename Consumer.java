d5b4cd560e901d6f7958c74b2ea762a63053d5f0
saimamidi01 <89061658+saimamidi01@users.noreply.github.com>
2025-03-04 16:45:45 -0800

    Implement basic sign up/login and skeleton for Forum class.

--- Consumer.java
+++ Consumer.java
@@ -1,3 +1,15 @@
 public class Consumer {
-    
+   private String username;
+   private String password;
+   public Consumer(String username, String password) {
+      this.username = username;
+      this.password = password;
+   }
+   public String getUsername() {
+    return username;
+   }
+   public String getPassword() {
+    return password;
+   }
+   
 }
--- Forum.java
+++ Forum.java
@@ -1,15 +1,95 @@
 import java.util.ArrayList;
 import java.util.Scanner;
 public class Forum {
-    ArrayList<Consumer> Consumer = new ArrayList<Consumer>();
+    static ArrayList<Consumer> Users = new ArrayList<Consumer>();
+    private static Consumer current = null;
+    static boolean home = true;
    public static void main(String[] args) {
-      displayHomeInterface();
-      
+       Scanner s = new Scanner(System.in);
+       Consumer current = null;
+       while(true) {
+         if(home) {
+           setup(s);
+         }
+         if(!home) {
+            functionality(s);
+         }
 
+       }
+
+    }
+   public static void setup(Scanner s) {
+     boolean doneSetup = false; 
+     while(!doneSetup) {
+        System.out.println("L: Login");
+        System.out.println("S: Sign Up");
+        System.out.println("EXIT: exits program(loses all data)");
+        System.out.println("Enter input based on key");
+        String input = s.nextLine();
+        if(input.equals("S")) {
+            System.out.println("Enter username: ");
+            String username = s.nextLine();
+            System.out.println("Enter password: ");
+            String password = s.nextLine();
+            Users.add(new Consumer(username, password));
+        }
+        else if(input.equals("L")) {
+            System.out.println("Enter username: ");
+            String username = s.nextLine();
+            System.out.println("Enter password: ");
+            String password = s.nextLine();
+            if(validateInfo(username, password)) {
+               doneSetup = true;
+               home = false;
+            }
+            else {
+                System.out.println("Incorrect password or username");
+                System.out.println("Try again!");
+            }
+            
+        }
+        else if(input.equals("EXIT")) {
+            System.exit(0);
+        }
+        
+     }
+     
    }
-   public static void displayHomeInterface() {
-     System.out.println("1.Login");
-     System.out.println("2. Sign Up");
-     System.out.println("3: EXIT (Lose all data)");
+   // please add a printline statement in the same format and an if statement.
+   // to implement functionality. 
+   public static void functionality(Scanner s) {
+       boolean done = false;
+      while(!done) {
+         System.out.println("mp: Make post");
+         System.out.println("vp: viewPosts");
+         System.out.println("vf: viewFriend");
+         System.out.println("af: add friend");
+         System.out.println("lo: logout");
+         System.out.println("EXIT: exits program(loses all data)");
+         System.out.println("Enter input(2 character): ");
+         String input = s.nextLine();
+         if(input.equals("lo")) {
+            logout();
+            done = true;
+         }
+         else if(input.equals("EXIT")) {
+            System.exit(0);
+         }
+          
+       }
    }
+   public static boolean validateInfo(String username, String password) {
+          for(Consumer s: Users) {
+             if(s.getUsername().equals(username) && s.getPassword().equals(password) ) {
+                 current = s;
+                 return true;
+             }
+          }
+          return false;
+   }
+   public static void logout() {
+      home = true; 
+      current = null;
+   }
+
 }
