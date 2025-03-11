6e5cd9725f5f1ca83849b3259f89c7a0195b255f
Steven Nguyen <stevensinguyen@gmail.com>
2025-03-11 14:52:08 -0700

    added addMember method to Group class

--- Group.java
+++ Group.java
@@ -4,10 +4,10 @@ public class Group  {
     private boolean searchable;
     private String name;
     private ArrayList<Consumer> groupMembers = new ArrayList<Consumer>();
+    private ArrayList<Consumer> moderators = new ArrayList<Consumer>(); // change to Moderator object when class is created
 
-    public Group(String name, ArrayList<> groupMembers) {
+    public Group(String name) {
         this.name = name;
-        this.groupMembers = groupMembers;
     }
     public String getGroupName() {
         return name;
@@ -16,4 +16,11 @@ public class Group  {
     public ArrayList<Consumer> getGroupMembers() {
         return groupMembers;
     }
-}
+
+    public void addMember(Consumer consumer) {
+        groupMembers.add(consumer);
+        System.out.println(consumer.getUsername() + " is added to the group.");
+    }
+
+    }
+
