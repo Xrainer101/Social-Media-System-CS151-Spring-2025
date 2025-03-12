53a99b09d7507d2bc327a0b6b06ec6397b5259d0
Xrainer101 <theiplumbers@gmail.com>
2025-03-11 18:06:28 -0700

    Interfaces Creation
    Created the interfaces we will use, with their abstract methods and Inviting's constant maxUsers.

---
+++ Manageable.java
@@ -0,0 +1,6 @@
+public interface Manageable {
+    public abstract void changeSettings();
+    public abstract void clear();
+    public abstract void metrics();
+    public abstract void delete();
+}
