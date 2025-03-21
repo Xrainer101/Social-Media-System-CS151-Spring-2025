e69df3123ff096c80f73706a4efc1baeae81e7b4
Xrainer101 <146404942+Xrainer101@users.noreply.github.com>
2025-03-21 13:11:41 -0700

    Filled out ChildUser and Consumer interaction
    Added overridden methods and made Consumer able to create and manage children.

--- ChildUser.java
+++ ChildUser.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Scanner;
 public class ChildUser extends User {
     private String username;
     private String password;
@@ -7,8 +8,144 @@ public class ChildUser extends User {
     public ChildUser(String username, String password) {
       this.username = username;
       this.password = password;
-      groupInvites = new ArrayList<Group>();
+      groupInvites = new ArrayList<>();
     }
 
-    
+    private void manageChildInvitations() {
+      boolean done = false;
+      while(!done) {
+        Scanner s = new Scanner(System.in);
+        System.out.println(groupInvites.toString());
+        System.out.println("INSPECT: Look at a specific invite");
+        System.out.println("ACCEPT ALL: Accept all invites");
+        System.out.println("REJECT ALL: Reject all invites");
+        System.out.println("EXIT: End invitation management");
+        String ans = s.nextLine();
+        if(ans.equals("INSPECT")) {
+          System.out.println("Enter number of invitation you want to inspect (starting at 0)");
+          int groupIndex = s.nextInt();
+          Group groupInvite = groupInvites.get(groupIndex);
+          System.out.println(groupInvite.toString());
+          System.out.println("Type ACCEPT to accept the invitation");
+          System.out.println("Type REJECT to reject the invitation");
+          ans = s.nextLine();
+          if(ans.equals("ACCEPT")) {
+            //Implement the same as however you guys accept a groupInvite in consumer
+
+          } else if(ans.equals("REJECT")) {
+            //I guess remove that invite from groupInvites
+          }
+        } else if(ans.equals("ACCEPT ALL")) {
+          //Go through all of them and accept them
+        } else if(ans.equals("REJECT ALL")) {
+          //clear groupInvites
+        } else if(ans.equals("EXIT")) {
+          done = true;
+        } else {
+          System.out.println("Invalid input");
+        }
+      }
+    }
+
+    @Override
+    public void changeSettings() {
+        //Perhaps there should be a change username or change password method
+        Scanner s = new Scanner(System.in);
+        boolean done = false;
+        
+        while(!done) {
+         System.out.println("UN: Change username");
+         System.out.println("PW: Change password");
+         System.out.println("GROUP: Approve Group Invites");
+         System.out.println("EXIT: End management");
+         String input = s.nextLine();
+         if(input.equals("UN")) {
+             System.out.println("Enter username: ");
+             username = s.nextLine();
+             System.out.println("Username successfully changed");
+             System.out.println();
+         }
+         else if(input.equals("PW")) {
+             System.out.println("Enter password: ");
+             password = s.nextLine();
+             System.out.println("Password successfully changed");
+             System.out.println();
+             
+         }
+         else if(input.equals("GROUP")) {
+            manageChildInvitations();
+         }
+         else if(input.equals("EXIT")) {
+             done = true;
+         } else {
+            System.out.println("Invalid input");
+         }
+    }
+  }
+
+  @Override
+   public String getUsername() {
+      return username;
+   }
+
+   @Override
+   public String getPassword() {
+      return password;
+   }
+
+   @Override
+   public ArrayList<Group> getGroupInvites() {
+      return groupInvites;
+   }
+
+   @Override
+   public void addGroupInvite(Group group) {
+      boolean duplicateName = false;
+      for (Group g : groupInvites) {
+         if (group.getGroupName().equals(g.getGroupName())) {
+            duplicateName = true;
+         }
+      }
+      if (duplicateName) {
+         System.out.println(username + " already has a pending invite to " + group.getGroupName());
+      } else if (!duplicateName) {
+         groupInvites.add(group);
+      }
+   }
+
+    @Override
+    public void clear() {
+      //delete all messages and posts and such  
+      throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public void metrics() {
+         //show number of messages and posts and such
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public void delete() {
+      //delete all references to user (from all of the lists that it is in)
+      throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public void message(User) {
+         //This was probably already implementing in one of your branches
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public void requestJoin(Group) {
+         //Probably already implemented somewhere else
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public void usersOnline() {
+         //Display all online users (though maybe this is a better forum method, in which case swap this out for a friending method)
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
 }
--- Consumer.java
+++ Consumer.java
@@ -1,39 +1,42 @@
 import java.util.ArrayList;
+import java.util.Scanner;
 public class Consumer extends User {
    private String username;
    private String password;
    private ArrayList<Group> groupInvites;
+   private ArrayList<ChildUser> children;
 
    public Consumer(String username, String password) {
       this.username = username;
       this.password = password;
-      groupInvites = new ArrayList<Group>();
+      groupInvites = new ArrayList<>();
    }
 
-   public String getUsername() {
-      return username;
+   public void makeChildAccount(String username, String password) {
+         Scanner s = new Scanner(System.in);   
+         System.out.println("Enter username: ");
+         String childUsername = s.nextLine();
+         System.out.println("Enter password: ");
+         String childPassword = s.nextLine();
+         ChildUser child = new ChildUser(childUsername, childPassword);
+         Users.add(child);
+         //Maybe implement usersGet method in forum
+         children.add(child);
+         System.out.println();
+         System.out.println("Child successful created");
+         System.out.println();
+         System.out.println();
    }
 
-   public String getPassword() {
-      return password;
-   }
-
-   public ArrayList<Group> getGroupInvites() {
-      return groupInvites;
-   }
-
-   public void addGroupInvite(Group group) {
-      boolean duplicateName = false;
-      for (Group g : groupInvites) {
-         if (group.getGroupName().equals(g.getGroupName())) {
-            duplicateName = true;
-         }
-      }
-      if (duplicateName) {
-         System.out.println(username + " already has a pending invite to " + group.getGroupName());
-      } else if (!duplicateName) {
-         groupInvites.add(group);
-      }
+   public void manageChildren(ChildUser child) {
+      Scanner s = new Scanner(System.in);   
+      System.out.println(children.toString());
+      //Change if it does not actually print out the children in a list
+      System.out.println("Enter index (a number) of child you want to manage, starting with 0");
+      int childIndex = s.nextInt();
+      ChildUser currChild = children.get(childIndex);
+      currChild.changeSettings();
+      System.out.println("Child successfuly managed");
    }
 
    public void acceptGroupInvite(Group group) {
@@ -49,4 +52,102 @@ public class Consumer extends User {
          System.out.println(username + " is not in the group " + group.getGroupName());
       }
    }
+
+   @Override
+   public String getUsername() {
+      return username;
+   }
+
+   @Override
+   public String getPassword() {
+      return password;
+   }
+
+   @Override
+   public ArrayList<Group> getGroupInvites() {
+      return groupInvites;
+   }
+
+   @Override
+   public void addGroupInvite(Group group) {
+      boolean duplicateName = false;
+      for (Group g : groupInvites) {
+         if (group.getGroupName().equals(g.getGroupName())) {
+            duplicateName = true;
+         }
+      }
+      if (duplicateName) {
+         System.out.println(username + " already has a pending invite to " + group.getGroupName());
+      } else if (!duplicateName) {
+         groupInvites.add(group);
+      }
+   }
+
+    @Override
+    public void changeSettings() {
+        //Perhaps there should be a change username or change password method
+        Scanner s = new Scanner(System.in);
+        boolean done = false;
+        
+        while(!done) {
+         System.out.println("UN: Change username");
+         System.out.println("PW: Change password");
+         System.out.println("EXIT: End management");
+         String input = s.nextLine();
+         if(input.equals("UN")) {
+             System.out.println("Enter username: ");
+             username = s.nextLine();
+             System.out.println("Username successfully changed");
+             System.out.println();
+         }
+         else if(input.equals("PW")) {
+             System.out.println("Enter password: ");
+             password = s.nextLine();
+             System.out.println("Password successfully changed");
+             System.out.println();
+             
+         }
+         else if(input.equals("EXIT")) {
+             done = true;
+         } else {
+            System.out.println("Invalid input");
+         }
+    }
+   }
+
+    @Override
+    public void clear() {
+      //delete all messages and posts and such  
+      throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public void metrics() {
+         //show number of messages and posts and such
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public void delete() {
+      //delete all references to user (from all of the lists that it is in)
+      throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public void message(User) {
+         //This was probably already implementing in one of your branches
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public void requestJoin(Group) {
+         //Probably already implemented somewhere else
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public void usersOnline() {
+         //Display all online users (though maybe this is a better forum method, in which case swap this out for a friending method)
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
 }
